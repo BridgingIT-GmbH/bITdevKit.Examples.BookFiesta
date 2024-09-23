b609c7183cfe58b6143b4da758b978459d4ba00b
Vincent van Proosdij <164303+vip32@users.noreply.github.com>
2024-09-23 23:25:54 +0200

    typo

--- src/Presentation.AppHost/Program.cs
+++ src/Presentation.AppHost/Program.cs
@@ -3,13 +3,16 @@
 // Use of this source code is governed by an MIT-style license that can be
 // found in the LICENSE file at https://github.com/bridgingit/bitdevkit/license
 
-using Projects;
-
 var builder = DistributedApplication.CreateBuilder(args);
 
-var sql = builder.AddSqlServer("sql", port: 14329).WithDataVolume().WithHealthCheck().AddDatabase("sqldata");
+var sql = builder.AddSqlServer("sql", port: 14329)
+    .WithDataVolume()
+    .WithHealthCheck()
+    .AddDatabase("sqldata");
 
-builder.AddProject<Presentation_Web_Server>("presentation-web-server").WaitFor(sql).WithReference(sql);
+builder.AddProject<Projects.Presentation_Web_Server>("presentation-web-server")
+    .WaitFor(sql)
+    .WithReference(sql);
 
 //TODO: add SEQ integration https://learn.microsoft.com/en-us/dotnet/aspire/logging/seq-integration?tabs=dotnet-cli
 
